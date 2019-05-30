3ea97814a19f717392728d9a6062489188e785f7
bonecrushereb <bonecrushereb@gmail.com>
2019-05-30 15:16:52 -0700

    fix icons with ios add redux to tabs

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux';
 
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
+import configureStore from './src/store/configureStore';
 
-Navigation.registerComponent("exploring.AuthScreen", () => AuthScreen );
-Navigation.registerComponent("exploring.SharePlaceScreen", () => SharePlaceScreen );
-Navigation.registerComponent("exploring.FindPlaceScreen", () => FindPlaceScreen );
+const store = configureStore();
+
+Navigation.registerComponent("locator.AuthScreen", () => AuthScreen, store, Provider );
+Navigation.registerComponent("locator.SharePlaceScreen", () => SharePlaceScreen, store, Provider );
+Navigation.registerComponent("locator.FindPlaceScreen", () => FindPlaceScreen, store, Provider );
 
 Navigation.startSingleScreenApp({
   screen: {
-    screen: "exploring.AuthScreen",
+    screen: "locator.AuthScreen",
     title: "Login"
   }
 });
--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,19 +1,27 @@
 import { Navigation } from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const startTabs = () => {
-  Navigation.startTabBasedApp({
-    tabs: [
-      {
-        screen: "exploring.FindPlaceScreen",
-        label: "Find Place",
-        title: "Find Place"
-      },
-      {
-        screen: "exploring.SharePlaceScreen",
-        label: "Share Place",
-        title: "Share Place"
-      }
-    ]
+  Promise.all([
+    Icon.getImageSource("md-map", 30),
+    Icon.getImageSource("ios-share-alt", 30)
+  ]).then(sources => {
+    Navigation.startTabBasedApp({
+      tabs: [
+        {
+          screen: "locator.FindPlaceScreen",
+          label: "Find Place",
+          title: "Find Place",
+          icon: sources[0]
+        },
+        {
+          screen: "locator.SharePlaceScreen",
+          label: "Share Place",
+          title: "Share Place",
+          icon: sources[1]
+        }
+      ]
+    });
   });
 };
 
