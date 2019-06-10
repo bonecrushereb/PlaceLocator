958b8569816915392a9cfa7d20627704a08cf725
bonecrushereb <bonecrushereb@gmail.com>
2019-06-10 15:07:26 -0700

    move delete functionality to react-redux

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
-import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail'
+import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
 import configureStore from './src/store/configureStore';
 
 const store = configureStore();
@@ -12,7 +12,7 @@ const store = configureStore();
 Navigation.registerComponent("locator.AuthScreen", () => AuthScreen, store, Provider );
 Navigation.registerComponent("locator.SharePlaceScreen", () => SharePlaceScreen, store, Provider );
 Navigation.registerComponent("locator.FindPlaceScreen", () => FindPlaceScreen, store, Provider );
-Navigation.registerComponent("locator.PlaceDetailScreen", () => PlaceDetailScreen);
+Navigation.registerComponent("locator.PlaceDetailScreen", () => PlaceDetailScreen, store, Provider);
 
 Navigation.startSingleScreenApp({
   screen: {
--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import { View, Image, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
-
+import { connect } from "react-redux";
 import Icon from 'react-native-vector-icons/Ionicons';
 
+import { deletePlace } from '../../store/actions/index';
 import styles from '../../../styles/styles';
 
-const placeDetailScreen = props => {
-  return (
+class PlaceDetail extends Component {
+  placeDeletedHandler = () => {
+    this.props.onDeletePlace(this.props.selectedPlace.key);
+    this.props.navigator.pop();
+  };
+  render () {
+    return (
       <View style={styles.placeDetailContainer}>
         <View>
-          <Image source={props.selectedPlace.image} style={styles.placeImageLg} />
-          <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
+          <Image source={this.props.selectedPlace.image} style={styles.placeImageLg} />
+        <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
         </View>
         <View>
-          <TouchableOpacity onPress={props.onItemDeleted}>
+          <TouchableOpacity onPress={this.placeDeletedHandler}>
             <View style={styles.deleteButton}>
               <Icon size={30} name="ios-trash" color="red"/>
             </View>
           </TouchableOpacity>
         </View>
       </View>
-  );
+    );
+  };
 };
 
-export default placeDetailScreen;
+const mapDispatchToProps = dispatch => {
+  return {
+    onDeletePlace: key => dispatch(deletePlace(key))
+  };
+};
+
+export default connect(null, mapDispatchToProps)(PlaceDetail);
